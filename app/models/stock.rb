41076905e94be751365ba3248f3f572d44cb86f1
Ryan Campione <ryan.campione@gmail.com>
2018-08-17 19:36:53 +0000

    added many to many relationship between users and stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
   
   def self.new_from_lookup(ticker_symbol)
     looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
@@ -16,6 +18,11 @@ class Stock < ApplicationRecord
         return nil
       end
   end
+  
+  def self.find_by_ticker(ticker_symbol)
+    
+  end
+  
   
   # def self.strip_commas(number)
   #   number.gsub(",", "")
