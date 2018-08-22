11115f197a3ec48d1d41b848597620cdf19baf94
Ryan Campione <ryan.campione@gmail.com>
2018-08-22 21:06:11 +0000

    updated views and navigation

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,17 @@ class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
   
+  before_save {
+    self.email = email.downcase
+  }
+  
+  # get user's full name
+  def full_name
+    return "#{first_name} #{last_name}".strip if (first_name || last_name)
+    "Anonymous"
+  end
+  
+  # determine if user already follows a given stock
   def stock_already_added?(ticker_symbol)
     stock = Stock.find_by_ticker(ticker_symbol)
     
@@ -17,12 +28,12 @@ class User < ApplicationRecord
     user_stocks.where(stock_id: stock.id).exists?
   end
   
-  
+  # determine if user follows less than 10 stocks
   def within_stock_limit?
     (user_stocks.count < 10)
   end
   
-  
+  # determine if a user can follow a given stock
   def can_add_stock?(ticker_symbol)
     within_stock_limit? && !stock_already_added?(ticker_symbol)
   end
