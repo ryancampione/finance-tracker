7973fe127f609aa39286fb4e89147e6f5d4ee3ee
Ryan Campione <ryan.campione@gmail.com>
2018-09-06 03:03:13 +0000

    added friendships

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
   
   get 'my_portfolio', to: "users#my_portfolio"
+  get 'my_friends', to: "users#my_friends"
   get 'search_stocks', to: "stocks#search"
+ 
   
   resources :user_stocks, only: [:create, :destroy]
 end
